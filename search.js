bc1a74f7582cf8a747c69fa732750acff2165260
Chuma Nande <chumanande80@gmail.com>
2026-03-01 00:18:54 +0000

    copied api

--- search.js
+++ search.js
@@ -3,7 +3,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "6o3abb692a1005b740713e4d4bftb103";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
---
+++ src/api.js
@@ -0,0 +1,10 @@
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let apiKey ="6o3abb692a1005b740713e4d4bftb103";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
